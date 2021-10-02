f4b9eaefacf7048a759161fecb6b3abf9cba31be
Guillermo <galemanz@ulasalle.edu.pe>
2021-10-02 02:05:22 -0500

    Create_1.0

--- gym/urls.py
+++ gym/urls.py
@@ -1,7 +1,20 @@
-from django.contrib import admin
+'''from django.contrib import admin
 from django.urls import include,path
 
 urlpatterns = [
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
+'''
+
+from django.contrib import admin  
+from django.urls import path  
+from polls import views  
+urlpatterns = [  
+    path('admin/', admin.site.urls),  
+    path('exc', views.exc),  
+    path('show',views.show),  
+    path('edit/<int:id>', views.edit),  
+    path('update/<int:id>', views.update),  
+    path('delete/<int:id>', views.destroy),  
+]  
