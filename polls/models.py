f93faba250f7efe4c19c7311faf8b873dfe23b9d
Guillermo <galemanz@ulasalle.edu.pe>
2021-10-02 01:21:40 -0500

    form

--- polls/models.py
+++ polls/models.py
@@ -2,8 +2,6 @@ import datetime
 
 from django.db import models
 
-
-
 class Exercise(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
