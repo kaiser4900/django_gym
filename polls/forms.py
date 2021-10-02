4492ef6c22de6273bfb2747b18127424a7b8d148
Guillermo <galemanz@ulasalle.edu.pe>
2021-10-02 01:25:42 -0500

    views

--- polls/forms.py
+++ polls/forms.py
@@ -1,6 +1,6 @@
 from django import forms  
 
-from .models import Exercise  
+from polls.models import Exercise  
 class ExerciseForm(forms.ModelForm):  
     class Meta:  
         model = Exercise  
--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,33 @@
-from django.shortcuts import render
-
-from django.http import HttpResponse
-
-
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+from django.shortcuts import render, redirect  
+from polls.forms import ExerciseForm  
+from polls.models import Exercise  
+# Create your views here.  
+def emp(request):  
+    if request.method == "POST":  
+        form = ExerciseForm(request.POST)  
+        if form.is_valid():  
+            try:  
+                form.save()  
+                return redirect('/show')  
+            except:  
+                pass  
+    else:  
+        form = ExerciseForm()  
+    return render(request,'index.html',{'form':form})  
+def show(request):  
+    employees = Exercise.objects.all()  
+    return render(request,"show.html",{'employees':employees})  
+def edit(request, id):  
+    employee = Exercise.objects.get(id=id)  
+    return render(request,'edit.html', {'employee':employee})  
+def update(request, id):  
+    employee = Exercise.objects.get(id=id)  
+    form = ExerciseForm(request.POST, instance = employee)  
+    if form.is_valid():  
+        form.save()  
+        return redirect("/show")  
+    return render(request, 'edit.html', {'employee': employee})  
+def destroy(request, id):  
+    employee = Exercise.objects.get(id=id)  
+    employee.delete()  
+    return redirect("/show")  
