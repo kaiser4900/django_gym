cf87de2ab95594dd1737136802d92df0ea5fc81a
Guillermo <galemanz@ulasalle.edu.pe>
2021-10-02 01:03:45 -0500

    django

---
+++ polls/forms.py
@@ -0,0 +1,13 @@
+
+from django import forms
+from .models import Exercise
+
+
+class ExerciseForm(forms.ModelForm):
+ 
+    class Meta:
+        model = Exercise
+ 
+        fields = [
+            "title",
+        ]
---
+++ polls/models.py
@@ -0,0 +1,12 @@
+import datetime
+
+from django.db import models
+from django.utils import timezone
+
+
+class Exercise(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    def __str__(self):
+        return self.question_text
+
